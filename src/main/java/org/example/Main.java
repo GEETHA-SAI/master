8ce3bac5d69bceb18f6aa3631bc20ea1c3b0c6b0
GEETHA-SAI <47322458+GEETHA-SAI@users.noreply.github.com>
2023-10-13 13:43:29 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,18 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController // Add the @RestController annotation to make this class a REST controller
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        System.out.println("hello docker!");
+    }
+
+    @GetMapping("/hello") // Define a simple GET endpoint
+    public String hello() {
+        return "Hello, Docker!"; // Change the response message
     }
 }
